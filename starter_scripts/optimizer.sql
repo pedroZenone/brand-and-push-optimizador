47d296b09ac62c4758e499f28e486a7b2fb59561
pedroZenone <pedro.zenone@gmail.com>
2023-09-22 12:17:37 -0300

    creation query

---
+++ starter_scripts/optimizer.sql
@@ -0,0 +1,15 @@
+create table optimizer(
+	id_run int,
+	id_item int,
+	cod_prod varchar(13),
+	folio  varchar(10),
+	numdpc varchar(30),
+	grupo varchar(10),
+	placa varchar(20),
+	id_vehiculo varchar(15),
+	name_vehiculo varchar(30),
+	price_warn int,
+	precio_grupo decimal(19,4),
+	precio_vehiculo decimal(19,4),
+	propiedad varchar(10)
+)
